78ce9fd958f8a48bc195bc11ca7c10d897c316c7
Wanderson Alves <wandersonalwes@hotmail.com>
2024-02-26 10:39:42 -0300

    feat: add sign up

--- next-auth-v5/actions/sign-up.ts
+++ next-auth-v5/actions/sign-up.ts
@@ -1,8 +1,12 @@
 'use server'
 
 import { z } from 'zod'
+import bcrypt from 'bcryptjs'
+
+import { prisma } from '@/lib/prisma'
 
 import { SignUpSchema } from '@/schemas/sign-up-schema'
+import { getUserByEmail } from '@/data/user'
 
 export const signUp = async (values: z.infer<typeof SignUpSchema>) => {
   const validatedFields = SignUpSchema.safeParse(values)
@@ -11,5 +15,20 @@ export const signUp = async (values: z.infer<typeof SignUpSchema>) => {
     return { error: 'E-mail e/ou senha inválido!' }
   }
 
-  return { success: 'E-mail enviado' }
+  const { email, name, password } = validatedFields.data
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) return { error: 'Já existe uma conta com este e-mail' }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  })
+
+  return { success: 'Usuário criado' }
 }
